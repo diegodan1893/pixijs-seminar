5389a878afb3a4cdec0de39f859745edd95ae85e
Diego Iáñez <diegodan1893@gmail.com>
2024-02-22 01:53:39 +0100

    Don't flash the throbber if load times are short

--- src/scenes/Throbber.ts
+++ src/scenes/Throbber.ts
@@ -18,6 +18,9 @@ class ThrobberSprite extends SpriteEntity {
 }
 
 export class Throbber extends Scene {
+	private showTimeout?: ReturnType<typeof setTimeout>
+	private showDelaySeconds: number = 0.1
+
 	get assetManifest() {
 		return {
 			assets: {
@@ -30,5 +33,35 @@ export class Throbber extends Scene {
 		const throbberSprite = new ThrobberSprite(this.assets.throbberImage)
 		throbberSprite.position.set(this.app.halfWidth, this.app.halfHeight)
 		this.addEntity(throbberSprite)
+	}
+
+	skipAllTransitions() {
+		if (this.showTimeout) {
+			clearTimeout(this.showTimeout)
+			this.showTimeout = undefined
+		}
+
+		super.skipAllTransitions()
+	}
+
+	async transitionIn() {
+		this.skipAllTransitions()
+
+		// Delay showing the spinner to avoid flashing the image
+		// if the load time is short enough.
+		// We use a timeout instead of awaiting sleep to allow cancelling
+		// the transition if transitionOut gets called before it completes.
+		this.showTimeout = setTimeout(
+			() => this.fade(1, this.defaultTransitionDurationSeconds),
+			this.showDelaySeconds * 1000
+		)
+	}
+
+	async transitionOut() {
+		this.skipAllTransitions()
+
+		if (this.alpha > 0) {
+			await this.fade(0, this.defaultTransitionDurationSeconds)
+		}
 	}
 }
