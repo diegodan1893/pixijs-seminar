079e88bd6c686c86f02dfccee377d4dece9d6f28
Diego Iáñez <diegodan1893@gmail.com>
2024-02-22 20:17:16 +0100

    Move slide text functions to the base slide class

--- src/scenes/slides/Slide.ts
+++ src/scenes/slides/Slide.ts
@@ -1,12 +1,63 @@
+import { Point, TextStyle } from "pixi.js"
 import { Scene } from "../Scene"
+import { TextEntity } from "@/entities/TextEntity"
+import { sleep } from "@/util/Time"
+import { easeOutCubic } from "@/math/Easing"
 
 export abstract class Slide extends Scene {
+	private titlePosition = new Point(100, 150)
+	private nextContentLinePosition = new Point(100, 300)
+	private contentLineSpacing = 65
+
+	private slideDistance = 100
+	private slideDurationSeconds = 0.5
+	private slideWaitSeconds = 0.1
+
+	private titleStyle: Partial<TextStyle> = {
+		fontFamily: "Roboto",
+		fontWeight: "800",
+		fontSize: 90,
+	}
+
+	private contentStyle: Partial<TextStyle> = {
+		fontFamily: "Roboto",
+		fontSize: 40,
+	}
+
 	async transitionIn() {
 		this.visible = true
 		this.runAndGoToNext()
 	}
 
 	protected async start() {}
+
+	protected addTitle(text: string): Promise<TextEntity> {
+		return this.addText(text, this.titleStyle, this.titlePosition)
+	}
+
+	protected addContentLine(text: string): Promise<TextEntity> {
+		const linePosition = this.nextContentLinePosition.clone()
+		this.nextContentLinePosition.y += this.contentLineSpacing
+
+		return this.addText(text, this.contentStyle, linePosition)
+	}
+
+	protected async addText(
+		text: string,
+		style: Partial<TextStyle>,
+		position: Point
+	): Promise<TextEntity> {
+		const line = new TextEntity(text, style)
+		line.position.set(position.x + this.slideDistance, position.y)
+		line.visible = false
+		this.addEntity(line)
+
+		line.fade(1, this.slideDurationSeconds)
+		line.move(position.x, line.y, this.slideDurationSeconds, easeOutCubic)
+		await sleep(this.slideWaitSeconds)
+
+		return line
+	}
 
 	private async runAndGoToNext() {
 		await this.start()
--- src/scenes/slides/TextSlide.ts
+++ src/scenes/slides/TextSlide.ts
@@ -1,30 +1,7 @@
 import { App } from "@/app/App"
 import { Slide } from "./Slide"
-import { TextEntity } from "@/entities/TextEntity"
-import { Point, TextStyle } from "pixi.js"
-import { easeOutCubic } from "@/math/Easing"
-import { sleep } from "@/util/Time"
 
 export class TextSlide extends Slide {
-	private titlePosition = new Point(100, 150)
-	private contentPosition = new Point(100, 300)
-	private lineSpacing = 10
-
-	private slideDistance = 100
-	private slideDurationSeconds = 0.5
-	private slideWaitSeconds = 0.1
-
-	private titleStyle: Partial<TextStyle> = {
-		fontFamily: "Roboto",
-		fontWeight: "800",
-		fontSize: 90,
-	}
-
-	private contentStyle: Partial<TextStyle> = {
-		fontFamily: "Roboto",
-		fontSize: 40,
-	}
-
 	constructor(app: App, private text: [string, ...string[]]) {
 		super(app)
 	}
@@ -38,34 +15,10 @@ export class TextSlide extends Slide {
 	protected async start(): Promise<void> {
 		const [titleText, ...contentText] = this.text
 
-		await this.addText(titleText, this.titleStyle, this.titlePosition)
+		await this.addTitle(titleText)
 
-		let lineY = this.contentPosition.y
 		for (const lineText of contentText) {
-			const line = await this.addText(
-				lineText,
-				this.contentStyle,
-				new Point(this.contentPosition.x, lineY)
-			)
-
-			lineY += line.height + this.lineSpacing
+			await this.addContentLine(lineText)
 		}
-	}
-
-	private async addText(
-		text: string,
-		style: Partial<TextStyle>,
-		position: Point
-	): Promise<TextEntity> {
-		const line = new TextEntity(text, style)
-		line.position.set(position.x + this.slideDistance, position.y)
-		line.visible = false
-		this.addEntity(line)
-
-		line.fade(1, this.slideDurationSeconds)
-		line.move(position.x, line.y, this.slideDurationSeconds, easeOutCubic)
-		await sleep(this.slideWaitSeconds)
-
-		return line
 	}
 }
